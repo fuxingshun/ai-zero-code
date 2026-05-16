93ffab36ba0c7f80be8e7f3fbc2946b803781a97
fxs6-hub <3922026286@qq.com>
2026-05-16 17:01:47 +0800

    性能优化 AI并发调用问题 多例模式解决

--- src/main/java/com/fxs/aizerocode/service/impl/AppServiceImpl.java
+++ src/main/java/com/fxs/aizerocode/service/impl/AppServiceImpl.java
@@ -6,6 +6,7 @@ import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.util.RandomUtil;
 import cn.hutool.core.util.StrUtil;
 import com.fxs.aizerocode.ai.AiCodeGenTypeRoutingService;
+import com.fxs.aizerocode.ai.AiCodeGenTypeRoutingServiceFactory;
 import com.fxs.aizerocode.constant.AppConstant;
 import com.fxs.aizerocode.core.AiCodeGeneratorFacade;
 import com.fxs.aizerocode.core.builder.VueProjectBuilder;
@@ -65,7 +66,7 @@ public class AppServiceImpl extends ServiceImpl<AppMapper, App>  implements AppS
     @Resource
     private ScreenshotService screenshotService;
     @Resource
-    private AiCodeGenTypeRoutingService aiCodeGenTypeRoutingService;
+    private AiCodeGenTypeRoutingServiceFactory aiCodeGenTypeRoutingServiceFactory;
 
 
 
@@ -82,7 +83,8 @@ public class AppServiceImpl extends ServiceImpl<AppMapper, App>  implements AppS
         // 应用名称暂时为 initPrompt 前 12 位
         app.setAppName(initPrompt.substring(0, Math.min(initPrompt.length(), 12)));
         // 使用 AI 智能选择代码生成类型
-        CodeGenTypeEnum selectedCodeGenType = aiCodeGenTypeRoutingService.routeCodeGenType(initPrompt);
+        AiCodeGenTypeRoutingService routingService = aiCodeGenTypeRoutingServiceFactory.createAiCodeGenTypeRoutingService();
+        CodeGenTypeEnum selectedCodeGenType = routingService.routeCodeGenType(initPrompt);
         app.setCodeGenType(selectedCodeGenType.getValue());
         // 插入数据库
         boolean result = this.save(app);
