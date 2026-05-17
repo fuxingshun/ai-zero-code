be3742693cf7655a8f0d1ef2d87bbda7cdba09a5
fxs6-hub <3922026286@qq.com>
2026-05-17 22:55:08 +0800

    性能优化：安全性优化 流量保护（使用Redisson分布式限流）

--- src/main/java/com/fxs/aizerocode/controller/AppController.java
+++ src/main/java/com/fxs/aizerocode/controller/AppController.java
@@ -16,6 +16,8 @@ import com.fxs.aizerocode.model.dto.app.*;
 import com.fxs.aizerocode.model.entity.User;
 import com.fxs.aizerocode.model.enums.CodeGenTypeEnum;
 import com.fxs.aizerocode.model.vo.app.AppVO;
+import com.fxs.aizerocode.ratelimit.annotation.RateLimit;
+import com.fxs.aizerocode.ratelimit.enums.RateLimitType;
 import com.fxs.aizerocode.service.ProjectDownloadService;
 import com.fxs.aizerocode.service.UserService;
 import com.mybatisflex.core.paginate.Page;
@@ -100,6 +102,7 @@ public class AppController {
      * @return 生成结果流
      */
     @GetMapping(value = "/chat/gen/code", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @RateLimit(limitType = RateLimitType.USER, rate = 5, rateInterval = 60, message = "AI 对话请求过于频繁，请稍后再试")
     public Flux<ServerSentEvent<String>> chatToGenCode(@RequestParam Long appId,
                                                        @RequestParam String message,
                                                        HttpServletRequest request) {
